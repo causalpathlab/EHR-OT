e8962d727d82f466f11a069842a20e39470f6761
Wanxin Li <w328li@uwaterloo.ca>
2024-03-06 21:45:58 -0800

    RSD works for MIMIC dataset

--- mimiciii_exp/admission/mimic_exp4_RSD.py
+++ mimiciii_exp/admission/mimic_exp4_RSD.py
@@ -26,27 +26,30 @@ import torch.optim as optim
 
 
 #################### Test using synthetic data ####################
+output_dir = os.path.join(os.path.expanduser("~"), f"OTTEHR/outputs/mimiciii")
+print(f"Will save outputs to {output_dir}")
 
-import numpy as np
+suffix = None
 
-# Generating synthetic data
-np.random.seed(0)  # For reproducibility
+source_count = 120
+target_count = 100
+iterations = 100
+trans_metric = 'deepJDOT'
 
-# Parameters
-n_source_samples = 1000
-n_target_samples = 2000
-n_features = 100
+# group_name = 'marital_status'
+# groups = ['MARRIED', 'SINGLE', 'WIDOWED', 'DIVORCED', 'SEPARATED']
 
-# Synthetic feature data
-source_data = np.random.rand(n_source_samples, n_features)
-target_data = np.random.rand(n_target_samples, n_features)
+group_name = 'insurance'
+groups = ['Self_Pay', 'Private', 'Government', 'Medicare', 'Medicaid']
 
-# Synthetic labels (target variables), assuming a simple linear relation with some noise
-weights = np.random.rand(n_features, 1)  # Random weights for our linear model
-source_labels = source_data @ weights + np.random.rand(n_source_samples, 1) * 5  # Adding some noise
-target_labels = target_data @ weights + np.random.rand(n_target_samples, 1) * 5  # Adding some noise
-
-source_data.shape, source_labels.shape, target_data.shape, target_labels.shape
+source = 'Self_Pay'
+target = 'Private'
+admid_diagnosis_df = pd.read_csv(os.path.join(output_dir, "ADMID_DIAGNOSIS.csv"), index_col=0, header=0, converters={'ICD codes': literal_eval})
+selected_df = select_samples(admid_diagnosis_df, group_name, source, target, source_count, target_count)
+code_feature_name = 'ICD codes'
+label_name = 'duration'
+source_data, source_labels, target_data, target_labels = gen_code_feature_label(selected_df, group_name, source, target, code_feature_name, label_name)
+print("print data dimensions:", source_data.shape, source_labels.shape, target_data.shape, target_labels.shape)
 
 # Convert the numpy arrays into torch tensors
 source_data_tensor = torch.tensor(source_data.astype(np.float32))
@@ -54,12 +57,12 @@ source_labels_tensor = torch.tensor(source_labels.astype(np.float32)).view(-1, 1
 target_data_tensor = torch.tensor(target_data.astype(np.float32))
 target_labels_tensor = torch.tensor(target_labels.astype(np.float32)).view(-1, 1)
 
-n_components = 10
+n_components = 50
 
 
 # Define the linear regression model with a feature extraction layer
 class LinearRegressionModel(nn.Module):
-    def __init__(self, input_features=source_data.shape[1], output_features=source_labels.shape[1], hidden_units=n_components):
+    def __init__(self, input_features=source_data.shape[1], output_features=1, hidden_units=n_components):
         super(LinearRegressionModel, self).__init__()
         # Feature extraction layers
         self.extraction = nn.Linear(input_features, hidden_units)  # First hidden layer
@@ -69,36 +72,17 @@ class LinearRegressionModel(nn.Module):
 
     def forward(self, x):
         # Apply non-linear transformations for feature extraction
-        print("x is:", x)
-        print("weights of extraction layer is:", self.extraction.weight.data)
-        print("x extraction is:", self.extraction(x))
+        # print("x is:", x)
+        # print("weights of extraction layer is:", self.extraction.weight.data)
+        # print("x extraction is:", self.extraction(x))
         feature = self.extraction(x)
 
         # Linear output
         x = self.output(feature)
-        print("weights of the output layer is:", self.output.weight.data)
+        # print("weights of the output layer is:", self.output.weight.data)
         return feature, x
 
 def RSD_BMP(source_feature, target_feature, RSD_coef = 0.001, BMP_coef = 0.1, eps=1e-6):
-    def smooth_approximation(x):
-        # Parameters for smooth transition
-        k = 10  # Controls the sharpness of the transition
-        epsilon = 1e-2  # Small value to avoid division by zero
-        
-        # Transition from 0 to x: sigmoid(k * x)
-        y_start = F.sigmoid(k * x) * x
-        
-        # Transition from x to 1: (1 - sigmoid(k * (x - 1)))
-        y_end = 1 - F.sigmoid(k * (x - 1))
-        
-        # Combine both parts
-        y = y_start * y_end
-        
-        # Ensure output is smoothed at boundaries
-        y = torch.where(x < epsilon, torch.zeros_like(x), y)  # Approximates y = 0 when x < 0
-        y = torch.where(x > 1 - epsilon, torch.ones_like(x), y)  # Approximates y = 1 when x > 1
-    
-        return y
 
     def softplus_abs(x, beta=10):
         # A smooth approximation of the absolute value function
@@ -113,26 +97,26 @@ def RSD_BMP(source_feature, target_feature, RSD_coef = 0.001, BMP_coef = 0.1, ep
         matrix_norm = (feature - mean) / (std + eps)  # Adding a small value to prevent division by zero
 
         # Add small Gaussian noise to the matrix to improve stability
-        noise_level = 1e-6  # Adjust based on your data and needs
+        noise_level = 1e-7  # Adjust based on your data and needs
         noise = torch.randn_like(matrix_norm) * noise_level
         noisy_feature = matrix_norm + noise
         return noisy_feature
 
     noisy_source_feature_t = add_noise(source_feature.t())
     noisy_target_feature_t = add_noise(target_feature.t())
-    print("noisy_source_feature_t is:", noisy_source_feature_t)
-    print("noisy_target_feature_t is:", noisy_target_feature_t)
+    # print("noisy_source_feature_t is:", noisy_source_feature_t)
+    # print("noisy_target_feature_t is:", noisy_target_feature_t)
 
     u_s, _, _ = torch.svd(noisy_source_feature_t)
     u_t, _, _ = torch.svd(noisy_target_feature_t)
-    print("u_s is:", u_s)
-    print("u_t is:", u_t)
+    # print("u_s is:", u_s)
+    # print("u_t is:", u_t)
     noisy_product = add_noise(torch.mm(u_s.t(), u_t))
-    print("noisy_product is:", noisy_product)
+    # print("noisy_product is:", noisy_product)
     p_s, cosine, p_t = torch.svd(noisy_product)
-    print("p_s is:", p_s)
-    print("p_t is:", p_t)
-    print("cospa is:", cosine)
+    # print("p_s is:", p_s)
+    # print("p_t is:", p_t)
+    # print("cospa is:", cosine)
     adjusted_cosine = torch.clamp(1 - torch.pow(cosine, 2), min=eps)
     sine = torch.sqrt(adjusted_cosine)
     soft_diff = softplus_abs(p_s - p_t)
@@ -147,7 +131,7 @@ criterion = nn.MSELoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 # Training loop
-num_epochs = 10
+num_epochs = 1
 for epoch in range(num_epochs):
     # Convert numpy arrays to torch variables for gradient computation
     source_input_var = Variable(source_data_tensor)
@@ -160,12 +144,12 @@ for epoch in range(num_epochs):
     # Get output from the model, given the inputs
     source_feature, source_outputs = model(source_input_var)
     target_feature, _ =  model(target_input_var)
-    print("source_feature is:", source_feature)
+    # print("source_feature is:", source_feature)
 
     # Get loss for the predicted output
     regression_loss = criterion(source_outputs, source_label_var)
     rsd_bmp_loss = RSD_BMP(source_feature, target_feature)
-    print("regression loss is:", regression_loss, "rsd_bmp_loss is:", rsd_bmp_loss)
+    # print("regression loss is:", regression_loss, "rsd_bmp_loss is:", rsd_bmp_loss)
     total_loss = regression_loss + rsd_bmp_loss
     
     # Get gradients w.r.t to parameters
@@ -173,31 +157,31 @@ for epoch in range(num_epochs):
 
    
 
-    print("before optimization, print out model parameters")
-    for name, param in model.named_parameters():
-        print("name is:", name)
-        print("param is:", param)
-        print("param.grad is:", torch.nan_to_num(param.grad))
+    # print("before optimization, print out model parameters")
+    # for name, param in model.named_parameters():
+    #     print("name is:", name)
+    #     print("param is:", param)
+    #     print("param.grad is:", torch.nan_to_num(param.grad))
 
     # Clip graidents
-    clip_value = 1e-6
+    clip_value = 1e-3
     torch.nn.utils.clip_grad_norm_(model.parameters(), clip_value)
-    print("after gradient clipping, print out model parameters")
-    for name, param in model.named_parameters():
-        print("name is:", name)
-        print("param is:", param)
-        print("param.grad is:", torch.nan_to_num(param.grad))
+    # print("after gradient clipping, print out model parameters")
+    # for name, param in model.named_parameters():
+    #     print("name is:", name)
+    #     print("param is:", param)
+    #     print("param.grad is:", torch.nan_to_num(param.grad))
 
     # Update parameters
     optimizer.step()
 
-    print("after optimiazation, print out model parameters")
-    for name, param in model.named_parameters():
-        print("name is:", name)
-        print("param is:", param)
-        print("param.grad is:", param.grad)
+    # print("after optimiazation, print out model parameters")
+    # for name, param in model.named_parameters():
+    #     print("name is:", name)
+    #     print("param is:", param)
+    #     print("param.grad is:", param.grad)
 
-    if epoch % 100 == 0:
+    if epoch % 1 == 0:
         print(f'Epoch {epoch+1}/{num_epochs}, Loss: {total_loss.item()}')
 
     
@@ -216,5 +200,7 @@ with torch.no_grad():  # We don't need gradients in the testing phase
     _, predicted_test = model(Variable(target_data_tensor))
     predicted_test = predicted_test.data.numpy()
     # predicted_test = model(Variable(target_data_tensor)).data.numpy()
-    test_loss = np.mean((predicted_test - target_labels) ** 2)
-    print(f'Testing Mean Squared Error: {test_loss}')
+    test_MSE = np.mean((predicted_test - target_labels) ** 2)
+    test_MAE = np.mean(np.abs(predicted_test - target_labels))
+    print(f'Testing log MSE: {np.log(test_MSE)}')
+    print(f'Testing log MAE: {np.log(test_MAE)}')
